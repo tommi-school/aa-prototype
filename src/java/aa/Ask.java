c315b2ada3e3069eb69ab5a5f1f74490e91dadbe
Tommi <me@tommi.co>
2013-01-13 11:59:41 +0800

    Modify models

--- src/java/aa/Ask.java
+++ src/java/aa/Ask.java
@@ -1,10 +1,12 @@
 package aa;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 // represents an Ask (in a sell order)
 public class Ask {
 
+  private int id;
   private String stock;
   private int price; // ask price
   private String userId; // user who made this sell order
@@ -17,8 +19,20 @@ public class Ask {
     this.userId = userId;
     this.date = new Date();
   }
+  
+  public Ask(int id, String stock, int price, String userId, Date date) {
+    this.id = id;
+    this.stock = stock;
+    this.price = price;
+    this.userId = userId;
+    this.date = date;
+  }
 
   // getters
+  public int getId() {
+    return id; 
+  }
+  
   public String getStock() {
     return stock;
   }
@@ -33,6 +47,31 @@ public class Ask {
 
   public Date getDate() {
     return date;
+  }
+
+  public void setId(int id) {
+     this.id = id;
+  }
+  
+  public void setStock(String stock) {
+     this.stock = stock;
+  }
+
+  public void setPrice(int price) {
+     this.price = price;
+  }
+
+  public void setUserId(String userId) {
+     this.userId = userId;
+  }
+
+  public void setDate(Date date) {
+     this.date = date; 
+  }
+  
+  public String getFormattedDate() {
+     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+     return sdf.format(date);
   }
 
   // toString
--- src/java/aa/Bid.java
+++ src/java/aa/Bid.java
@@ -1,10 +1,12 @@
 package aa;
 
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 // represents a bid (in a buy order)
 public class Bid {
 
+  private int id;
   private String stock;
   private int price; // bid price
   private String userId; // user who made this buy order
@@ -17,8 +19,20 @@ public class Bid {
     this.userId = userId;
     this.date = new Date();
   }
+  
+  public Bid(int id, String stock, int price, String userId, Date date) {
+    this.id = id;
+    this.stock = stock;
+    this.price = price;
+    this.userId = userId;
+    this.date = date;
+  }
 
   // getters
+  public int getId() {
+    return id;
+  }
+  
   public String getStock() {
     return stock;
   }
@@ -35,6 +49,32 @@ public class Bid {
     return date;
   }
 
+  //setters
+  public void setId(int id) {
+      this.id = id;
+  }
+  
+  public void setStock(String stock) {
+      this.stock = stock;
+  }
+
+  public void setPrice(int price) {
+      this.price = price;
+  }
+
+  public void setUserId(String userId) {
+      this.userId = userId;
+  }
+
+  public void setDate(Date date) {
+      this.date = date;
+  }
+  
+  public String getFormattedDate() {
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      return sdf.format(date);
+  }
+ 
   // toString
   public String toString() {
     return "stock: " + stock + ", price: " + price + ", userId: " + userId + ", date: " + date;
